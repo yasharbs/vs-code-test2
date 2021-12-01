61cb19f61eb4b8e1f3231fc87676950d5ede7c4a
Yashar Balazadegan Sarvrood <yashar.balazadegan@gmail.com>
2021-12-01 11:37:49 -0700

    -main.cpp + testing the eigen + printing some outputs -test.h +include eigen library

---
+++ project1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<Eigen/Dense>
+#include"test.h"
+int main(int, char**) {
+    std::cout << "Hello vv,dddddd world!\n";
+    test obj(3,8);
+    std::cout<<obj.add()<<std::endl;
+        std::cout<<obj.add()<<std::endl;
+
+}
---
+++ project1/test.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<Eigen/Dense>
+
+class test
+{
+
+    private:
+    Eigen::Matrix2cd a;
+    Eigen::Matrix2cd b;
+
+int x,y;
+double z;
+public:
+test(int x, int y);
+int add();
+
+    
+};
